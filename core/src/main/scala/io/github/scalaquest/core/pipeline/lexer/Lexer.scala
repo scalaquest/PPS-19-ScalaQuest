46d6f00dcfc5930352c24c9e17762416abaeb6e5
Lippo <filippo.nardini@studio.unibo.it>
2021-01-03 17:14:55 +0100

    Add lexer scaladoc

--- core/src/main/scala/io/github/scalaquest/core/pipeline/lexer/Lexer.scala
+++ core/src/main/scala/io/github/scalaquest/core/pipeline/lexer/Lexer.scala
@@ -2,11 +2,13 @@ package io.github.scalaquest.core.pipeline.lexer
 
 import io.github.scalaquest.core.pipeline.lexer.Lexer.Token
 
-trait LexerResult {
-  def tokens: Seq[Token]
-}
-
+/** A lexer that accepts finite sequence of characters and cannot fail. */
 trait Lexer {
+
+  /** Performs the lexical analysis on a sequence of tokens.
+    * @param rawSentence the input finite sequence of characters
+    * @return the result of the lexical analysis operation
+    */
   def tokenize(rawSentence: String): LexerResult
 }
 
@@ -14,9 +16,19 @@ object Lexer {
   type Token = String
 }
 
+/** Result of the lexical analysis operation. */
+trait LexerResult {
+
+  /** Finite sequence of tokens. */
+  def tokens: Seq[Token]
+}
+
+/** @inheritdoc */
 case class SimpleLexerResult(tokens: Seq[Token]) extends LexerResult
 
+/** @inheritdoc */
 case object SimpleLexer extends Lexer {
+
   override def tokenize(rawSentence: String): LexerResult =
     SimpleLexerResult((rawSentence.toLowerCase split " ").filter(_ != "").toSeq)
 }
