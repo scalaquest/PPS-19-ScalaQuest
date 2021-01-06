12344f1ae1df8975c834b96024c8b8c972889a47
Lippo <filippo.nardini@studio.unibo.it>
2021-01-06 10:21:37 +0100

    Add tuProlog version 3.3.0

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,5 +6,6 @@ dependencies {
     // todo more module-specific dependencies here
 
     // prolog
-    implementation("it.unibo.alice.tuprolog:tuprolog:_")
+    implementation("it.unibo.alice.tuprolog:tuprolog:3.3.0")
+
 }
