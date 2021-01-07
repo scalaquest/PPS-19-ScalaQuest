1918dff674324ca21d2d67f0f3cfbeb6ee33b2e7
Lippo <filippo.nardini@studio.unibo.it>
2021-01-07 16:14:23 +0100

    Add Engine tests

--- core/src/test/scala/io/github/scalaquest/core/parsing/engine/EngineTest.scala
+++ core/src/test/scala/io/github/scalaquest/core/parsing/engine/EngineTest.scala
@@ -1,34 +1,106 @@
 package io.github.scalaquest.core.parsing.engine
 
-import alice.tuprolog.{Prolog, Theory, Term => TuPrologTerm}
-import org.scalatest.PrivateMethodTester.PrivateMethod
+import alice.tuprolog.Theory
+import org.scalatest.Inspectors._
 import org.scalatest.wordspec.AnyWordSpec
-import org.scalatest.PrivateMethodTester._
 
 class EngineTest extends AnyWordSpec {
 
-  val prolog: Prolog = new Prolog()
-  prolog.setTheory(
-    new Theory("""
-               |ciao(goro).
-               |ciao(filo).
-               |ciao(riechi).
-               |ciao(uoomo).
+  val theory = new Theory("""
+               |male(alessio).
+               |male(boris).
+               |male(claudio).
+               |female(daria).
+               |human(X) :- male(X).
+               |human(X) :- female(X).
                |""".stripMargin)
-  )
 
-  val engine: PrologEngine = PrologEngine(prolog)
+  val engine: Engine = Engine(theory)
 
   "The engine" when {
-    "provided any query" should {
-      "never throw exception" in {
-        val term: Term = Compound(Atom("ciao"), Variable("X"))
-        assert(engine.query(term).flatMap(_.getTerm(Variable("X"))).collect { case Atom(name) => name } == Seq(
-          "goro",
-          "filo",
-          "riechi",
-          "uoomo"
-        ))
+    "provided a query with a single result" should {
+      "return it as a unary sequence" in {
+        val term = Compound(Atom("male"), Atom("boris"))
+        val res  = engine.query(term)
+        assert(res.size == 1)
+        assert(res.head.body == term)
+      }
+    }
+    "provided a query with multiple results" should {
+      "return them as a sequence" in {
+        val term = Compound(Atom("human"), Variable("X"))
+        val res  = engine.query(term)
+        forAll(res.zip(Seq(
+          "alessio",
+          "boris",
+          "claudio",
+          "daria"
+        ))) { case (sol, name) =>
+          assert(sol.body == term.copy(arg1 = Atom(name)))
+        }
+      }
+      "resolve variables with their values" in {
+        val term = Compound(Atom("human"), Variable("X"))
+        val res  = engine.query(term)
+        forAll(res.zip(Seq(
+          "alessio",
+          "boris",
+          "claudio",
+          "daria"
+        ))) { case (sol, name) =>
+          assert(sol.getVariable(Variable("X")).contains(Atom(name)))
+        }
+      }
+      "resolve empty for unknown variables" in {
+        val term = Compound(Atom("human"), Variable("X"))
+        val res  = engine.query(term)
+        assert(res.nonEmpty)
+        assert(res.flatMap(_.getVariable(Variable("Y"))).isEmpty)
+      }
+    }
+  }
+
+  "Implicit tuProlog terms conversion" when {
+    import Engine.EnhancedTuPrologTerm
+    "passed a tuProlog atom" should {
+      "convert it to an Atom" in {
+        val atom = alice.tuprolog.Term.createTerm("hello")
+        assert(atom.isAtom)
+        assert(atom.toTerm == Atom("hello"))
+      }
+    }
+    "passed a tuProlog compound" should {
+      "convert it to a Compound" in {
+        val compound = alice.tuprolog.Term.createTerm("hello(world)")
+        assert(compound.toTerm == Compound(Atom("hello"), Atom("world")))
+      }
+    }
+    "passed a tuProlog number" should {
+      "convert it to a Number" in {
+        val number = new alice.tuprolog.Int(5)
+        assert(number.toTerm == Number(5))
+      }
+    }
+  }
+
+  "Implicit term conversion" when {
+    import Engine.EnhancedTerm
+    "provided an Atom" should {
+      "create a tuProlog atom" in {
+        val atom = Atom("hello")
+        assert(atom.toTuPrologTerm.isEqual(new alice.tuprolog.Struct("hello")))
+      }
+    }
+    "provided a Number" should {
+      "create a tuProlog number" in {
+        val number = Number(5)
+        assert(number.toTuPrologTerm.isEqual(new alice.tuprolog.Int(5)))
+      }
+    }
+    "provided a Compound" should {
+      "create a tuProlog compound" in {
+        val compound = Compound(Atom("hello"), Atom("world"))
+        assert(compound.toTuPrologTerm.isEqual(new alice.tuprolog.Struct("hello", new alice.tuprolog.Struct("world"))))
       }
     }
   }
