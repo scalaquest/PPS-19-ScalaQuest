3aa955c5009c35e2574d6d29c2e4d60fc6571870
Lippo <filippo.nardini@studio.unibo.it>
2021-01-08 18:39:40 +0100

    Refactor prolog source code, split it and put it into resources

---
+++ core/src/main/resources/names.pl
@@ -0,0 +1,20 @@
+name(ugo) --> [ugo].
+name(giuseppe) --> [giuseppe].
+name(marco) --> [marco].
+
+name(gift) --> [gift].
+name(key) --> [key].
+name(door) --> [door].
+name(water) --> [water].
+name(apple) --> [apple].
+
+tv(X^Y^use(Y,X)) --> [use].
+tv(X^Y^take(Y,X)) --> [take].
+tv(X^Y^pick_up(Y,X)) --> [pick, up].
+
+v(2/to, Y^X^go(X, Y)) --> [go].
+v(3/to, Z^Y^X^give(X,Y,Z) ) --> [give].
+v(3/from, Z^Y^X^buy(X,Y,Z) ) --> [buy].
+v(3/into, Z^Y^X^insert(X,Y,Z)) --> [insert].
+v(3/with, Z^Y^X^hit(X,Y,Z)) --> [hit].
+v(3/from, Z^Y^X^hitfrom(X,Y,Z)) --> [hit].
