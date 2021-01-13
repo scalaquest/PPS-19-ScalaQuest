0d04a4007c394967ffe352fe791110d37a6c8ce6
ThomasAngeliniUnibo <thomas.angelini@studio.unibo.it>
2021-01-13 11:57:47 +0100

    Added eatable behavior

--- core/src/main/scala/io/github/scalaquest/core/model/common/behaviors/CommonBehaviors.scala
+++ core/src/main/scala/io/github/scalaquest/core/model/common/behaviors/CommonBehaviors.scala
@@ -2,6 +2,7 @@ package io.github.scalaquest.core.model.common.behaviors
 
 import io.github.scalaquest.core.model.std.BehaviorableModel
 import io.github.scalaquest.core.model.Model
+import io.github.scalaquest.core.model.Room
 
 /**
  * When mixed into a [[Model]], it integrates into it the interfaces for some commonly used
@@ -21,7 +22,7 @@ trait CommonBehaviors extends BehaviorableModel {
     abstract class Takeable extends Behavior
 
     /**
-     * A [[Behavior]] associated to an [[Item]] that that can be opened and closed. Open and close actions should be
+     * A [[Behavior]] associated to an [[Item]] that can be opened and closed. Open and close actions should be
      * mutually exclusives.
      */
     abstract class Openable extends Behavior {
@@ -33,5 +34,11 @@ trait CommonBehaviors extends BehaviorableModel {
      * Conceptually, an [[Item]] that exposes a [[RoomLink]] behavior could also be [[Openable]].
      */
     abstract class RoomLink extends Behavior
+
+    /**
+     * A [[Behavior]] associated to an [[Item]] that can be eaten. When item is eaten shouldn't be more present
+     * in the player's bag or in the current [[Room]].
+     */
+    abstract class Eatable extends Behavior
   }
 }
