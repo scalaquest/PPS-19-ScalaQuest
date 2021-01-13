fe8061c34fb50203bbf9d399a51f66f5e8e05d72
ThomasAngeliniUnibo <thomas.angelini@studio.unibo.it>
2021-01-13 12:33:45 +0100

    Added Eatable Behavior

--- core/src/main/scala/io/github/scalaquest/core/model/common/behaviors/StdCommonBehaviors.scala
+++ core/src/main/scala/io/github/scalaquest/core/model/common/behaviors/StdCommonBehaviors.scala
@@ -1,11 +1,11 @@
 package io.github.scalaquest.core.model.common.behaviors
 
 import io.github.scalaquest.core.model.Model
-import io.github.scalaquest.core.model.common.behaviors.std.{Openable, RoomLink, Takeable}
+import io.github.scalaquest.core.model.common.behaviors.std.{Eatable, Openable, RoomLink, Takeable}
 
 /**
  * When mixed into a [[Model]], it enables the implementation for the common behaviors provided by ScalaQuest Core.
  * It requires the storyteller to implement all the required [[monocle.Lens]], used by the implementation to access
  * and re-generate the concrete [[Model.State]].
  */
-trait StdCommonBehaviors extends StdCommonBehaviorsBase with Takeable with Openable with RoomLink
+trait StdCommonBehaviors extends StdCommonBehaviorsBase with Takeable with Eatable with Openable with RoomLink
