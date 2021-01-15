70b5267cb60126f4e52fdc5d297ff6f7ca528158
Lippo <filippo.nardini@studio.unibo.it>
2021-01-15 17:47:07 +0100

    Move 'CompoundBuilder' in its file

---
+++ core/src/main/scala/io/github/scalaquest/core/parsing/scalog/dsl/CompoundBuilder.scala
@@ -0,0 +1,32 @@
+package io.github.scalaquest.core.parsing.scalog.dsl
+
+import io.github.scalaquest.core.parsing.scalog.{Atom, Compound, Term}
+
+/**
+ * A `CompoundBuilder` allows the generation of compound terms using a
+ * Prolog-like syntax that is checked by the scala compiler.
+ *
+ * @param functor the atom used as a functor by the generated compound terms
+ *
+ *  Example:
+ *  {{{
+ *    val hello = CompoundBuilder(Atom("hello"))
+ *    (hello(Atom("darkness"), Atom("my"), Atom("old"), Atom("friend"))).generate
+ *  }}}
+ *
+ *  Should result in the following compound term being printed:
+ *  {{{
+ *    hello(darkness, my, old, friend)
+ *  }}}
+ */
+case class CompoundBuilder(functor: Atom) {
+
+  /**
+   * Allows to simulate the Prolog compound term declaration.
+   * @param arg the first mandatory term
+   * @param args the other optional terms
+   * @return a [[Compound]] which has `functor` as functor and the arguments
+   *         as the compound term arguments.
+   */
+  def apply(arg: Term, args: Term*): Compound = Compound(functor, arg, args.toList)
+}
--- core/src/main/scala/io/github/scalaquest/core/parsing/scalog/dsl/package.scala
+++ core/src/main/scala/io/github/scalaquest/core/parsing/scalog/dsl/package.scala
@@ -20,34 +20,6 @@ package io.github.scalaquest.core.parsing.scalog
  */
 package object dsl {
 
-  /**
-   * A `CompoundBuilder` allows the generation of compound terms using a
-   * Prolog-like syntax that is checked by the scala compiler.
-   * @param functor the atom used as a functor by the generated compound terms
-   *
-   *  Example:
-   *  {{{
-   *    val hello = CompoundTerm(Atom("hello"))
-   *    (hello(Atom("darkness"), Atom("my"), Atom("old"), Atom("friend"))).generate
-   *  }}}
-   *
-   *  Should result in the following compound term being printed:
-   *  {{{
-   *    hello(darkness, my, old, friend)
-   *  }}}
-   */
-  case class CompoundBuilder(functor: Atom) {
-
-    /**
-     * Allows to simulate the Prolog compound term declaration.
-     * @param arg the first mandatory term
-     * @param args the other optional terms
-     * @return a [[Compound]] which has `functor` as functor and the arguments
-     *         as the compound term arguments.
-     */
-    def apply(arg: Term, args: Term*): Compound = Compound(functor, arg, args.toList)
-  }
-
   /** Enables the implicit conversion from `String` to `Atom` */
   implicit def stringToAtom(name: String): Atom = Atom(name)
 
