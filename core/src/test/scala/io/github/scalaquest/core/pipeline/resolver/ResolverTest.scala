994bf4d43e4ffa8302715f93b790ac0f1fd85239
Riccardo Maldini <riccardo.maldini@gmail.com>
2021-01-15 21:12:56 +0100

    Clean ResolverTest

--- core/src/test/scala/io/github/scalaquest/core/pipeline/resolver/ResolverTest.scala
+++ core/src/test/scala/io/github/scalaquest/core/pipeline/resolver/ResolverTest.scala
@@ -2,122 +2,109 @@ package io.github.scalaquest.core.pipeline.resolver
 
 import io.github.scalaquest.core.model.Action
 import io.github.scalaquest.core.model.common.Actions
+import io.github.scalaquest.core.model.common.Actions.{Open, Take}
 import io.github.scalaquest.core.pipeline.interpreter.ItemRef
 import io.github.scalaquest.core.pipeline.parser.{AST, ParserResult}
 import org.scalatest.wordspec.AnyWordSpec
 
 class ResolverTest extends AnyWordSpec {
   "A Resolver" when {
-    "receives a ParserResult" when {
-      val actionsMap      = Map[String, Action](("open", Actions.Open), ("close", Actions.Close))
-      val genericItemRef  = new ItemRef {}
-      val genericItemRef2 = new ItemRef {}
-      val itemsMap =
-        Map[String, ItemRef](("myItem", genericItemRef), ("myItemAlias", genericItemRef), ("myItem2", genericItemRef2))
-      val resolver = Resolver(actionsMap, itemsMap)
+    val actionsMap = Map[String, Action](
+      "take"  -> Actions.Take,
+      "bring" -> Actions.Take,
+      "open"  -> Actions.Open
+    )
 
-      "parser result is intransitive" should {
-        val parserResult = new ParserResult() {
-          override def tree: AST = AST.Intransitive("open", "")
-        }
-        val statement = for {
-          resolverResult <- resolver resolve parserResult
-        } yield resolverResult.statement
-        "produce an intransitive statement" in {
+    val appleItemRef = new ItemRef {}
+    val keyItemRef   = new ItemRef {}
+    val doorItemRef  = new ItemRef {}
+    val itemsMap = Map[String, ItemRef](
+      "apple"     -> appleItemRef,
+      "red apple" -> appleItemRef,
+      "door"      -> doorItemRef,
+      "key"       -> keyItemRef
+    )
 
-          assert(statement.isRight, "Resolver has not produced any statement")
-          assert(
-            statement.exists {
-              case Statement.Intransitive(_) => true
-              case _                         => false
-            },
-            "Resolver has not produced an intransitive statement"
-          )
+    val resolver = Resolver(actionsMap, itemsMap)
 
-        }
-        "contain expected values in content" in {
-          assert(
-            statement.contains(Statement.Intransitive(actionsMap("open"))),
-            "Resolver has not produced " +
-              "the correct statement"
-          )
-        }
+    "receives an Intransitive AST" should {
+      // fixme to adjust after team 1 merging
+      val parserResult = new ParserResult() {
+        override def tree: AST = AST.Intransitive("open", "you")
       }
 
-      "parser result is transitive" should {
-        val parserResult = new ParserResult() {
-          override def tree: AST = AST.Transitive("close", "", "myItem")
-        }
-        val statement = for {
-          resolverResult <- resolver resolve parserResult
-        } yield resolverResult.statement
+      val maybeStatement = resolver.resolve(parserResult).map(x => x.statement)
 
-        "produce a transitive statement" in {
+      "produce the right Intransitive Statement" in {
+        for { _ <- maybeStatement.left } yield fail("Resolver has not produced any statement")
 
-          assert(statement.isRight, "Resolver has not produced any statement")
-          assert(
-            statement.exists {
-              case Statement.Transitive(_, _) => true
-              case _                          => false
-            },
-            "Resolver has not produced a transitive statement"
-          )
-
-        }
-
-        "contain expected values in content" in {
-          assert(
-            statement.contains(Statement.Transitive(actionsMap("close"), itemsMap("myItem"))),
-            "Resolver has not produced " +
-              "the correct statement"
-          )
-
+        maybeStatement map {
+          case Statement.Intransitive(action) if action == Open =>
+            succeed
+          case Statement.Intransitive(_) =>
+            fail("Resolver has produced a wrong Intransitive Statement")
+          case _ =>
+            fail("Resolver has not produced an Intransitive Statement")
         }
       }
+    }
 
-      "parser result is ditransitive" should {
-        val parserResult = new ParserResult() {
-          override def tree: AST = AST.Ditransitive("open", "", "myItem", "myItem2")
-        }
-        val statement = for {
-          resolverResult <- resolver resolve parserResult
-        } yield resolverResult.statement
-        "produce a ditransitive statement" in {
+    "receives an Transitive AST" should {
+      // fixme to adjust after team 1 merging
+      val parserResult = new ParserResult() {
+        override def tree: AST = AST.Transitive("take", "you", "apple")
+      }
+      val maybeStatement = resolver.resolve(parserResult).map(x => x.statement)
 
-          assert(statement.isRight, "Resolver has not produced any statement")
-          assert(
-            statement.exists {
-              case Statement.Ditransitive(_, _, _) => true
-              case _                               => false
-            },
-            "Resolver has not produced a ditransitive statement"
-          )
+      "produce the right Transitive Statement" in {
+        for { _ <- maybeStatement.left } yield fail("Resolver has not produced any statement")
 
-        }
-        "contain expected values in content" in {
-          assert(
-            statement.contains(Statement.Ditransitive(actionsMap("open"), itemsMap("myItem"), itemsMap("myItem2"))),
-            "Resolver has not produced " +
-              "the correct statement"
-          )
-
+        maybeStatement map {
+          case Statement.Transitive(action, obj) if action == Take && obj == appleItemRef =>
+            succeed
+          case Statement.Transitive(_, _) =>
+            fail("Resolver has produced a wrong Transitive Statement")
+          case _ =>
+            fail("Resolver has not produced a Transitive Statement")
         }
       }
+    }
 
-      "parser result is type unknown" should {
-        "fail" in {
-          val parserResult = new ParserResult() {
-            override def tree: AST = null
-          }
-          val statement = for {
-            resolverResult <- resolver resolve parserResult
-          } yield resolverResult.statement
-          assert(
-            statement.left.getOrElse("non-left") == "The statement is wrong.",
-            "parser result is non empty but it should be"
-          )
+    "receives a Ditransitive AST" should {
+      // fixme to adjust after team 1 merging
+      val parserResult = new ParserResult() {
+        override def tree: AST = AST.Ditransitive("open", "you", "door", "key")
+      }
+      val maybeStatement = resolver.resolve(parserResult).map(x => x.statement)
 
+      "produce the right Ditransitive Statement" in {
+        for { _ <- maybeStatement.left } yield fail("Resolver has not produced any statement")
+
+        maybeStatement map {
+          case Statement.Ditransitive(action, dirObj, sideObj)
+              if action == Open && dirObj == doorItemRef && sideObj == keyItemRef =>
+            succeed
+          case Statement.Ditransitive(_, _, _) =>
+            fail("Resolver has produced a wrong Ditransitive Statement")
+          case _ =>
+            fail("Resolver has not produced a Ditransitive Statement")
         }
+      }
+    }
+
+    "receives an invalid AST" should {
+      // fixme to adjust after team 1 merging
+      "produce a Left with a description of what went wrong" in {
+        val parserResult = new ParserResult() {
+          override def tree: AST = null
+        }
+
+        val maybeStatement = resolver.resolve(parserResult).map(x => x.statement)
+
+        maybeStatement.fold(
+          left => assert(left == "The statement is wrong.", "The left description is invalid."),
+          _ => fail("Parser result is non empty, but it should be")
+        )
       }
     }
   }
