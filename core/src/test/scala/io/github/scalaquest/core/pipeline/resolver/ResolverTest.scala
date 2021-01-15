688ef11f23cbfcb38f4b295e4c9f3488d294a37a
Jacopo Corina <jacopo.corina@studio.unibo.it>
2021-01-15 15:55:17 +0100

    resolver intransitive test

--- core/src/test/scala/io/github/scalaquest/core/pipeline/resolver/ResolverTest.scala
+++ core/src/test/scala/io/github/scalaquest/core/pipeline/resolver/ResolverTest.scala
@@ -1,3 +1,48 @@
 package io.github.scalaquest.core.pipeline.resolver
 
-class ResolverTest {}
+import io.github.scalaquest.core.model.Action
+import io.github.scalaquest.core.model.common.Actions
+import io.github.scalaquest.core.pipeline.interpreter.ItemRef
+import io.github.scalaquest.core.pipeline.parser.{AST, ParserResult}
+import org.scalatest.wordspec.AnyWordSpec
+
+class ResolverTest extends AnyWordSpec {
+  "A Resolver" when {
+    "receives a ParserResult" when {
+      val actionsMap = Map[String, Action](("open", Actions.Open), ("close", Actions.Close))
+      val itemsMap   = Map[String, ItemRef]() //Map[String,ItemRef](("door",ItemRetriever()))
+      val resolver   = Resolver(actionsMap, itemsMap)
+
+      "parser result is intransitive" should {
+        val parserResult = new ParserResult() {
+          override def tree: AST = AST.Intransitive("open", "")
+        }
+        val statement = for {
+          resolverResult <- resolver resolve parserResult
+        } yield resolverResult.statement
+        "produce an intransitive statement" in {
+
+          assert(statement.isRight, "Resolver has not produced any statement")
+          assert(
+            statement.exists {
+              case Statement.Intransitive(_) => true
+              case _                         => false
+            },
+            "Resolver has not produced an intransitive statement"
+          )
+
+        }
+        "intransitive statement's content " must {
+          "be correct" in {
+            assert(
+              statement.contains(Statement.Intransitive(actionsMap("open"))),
+              "Resolver has not produced " +
+                "the correct statement"
+            )
+          }
+        }
+
+      }
+    }
+  }
+}
