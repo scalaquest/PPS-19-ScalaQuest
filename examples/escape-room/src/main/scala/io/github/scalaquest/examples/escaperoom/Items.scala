c00c130d004a1339baffd476a8e8a9e4189119e9
Jacopo Corina <jacopo.corina@studio.unibo.it>
2021-01-26 16:55:13 +0100

    messageLens usage

--- examples/escape-room/src/main/scala/io/github/scalaquest/examples/escaperoom/Items.scala
+++ examples/escape-room/src/main/scala/io/github/scalaquest/examples/escaperoom/Items.scala
@@ -1,14 +1,20 @@
 package io.github.scalaquest.examples.escaperoom
 
 import io.github.scalaquest.core.model.{ItemDescription, ItemRef}
+import io.github.scalaquest.examples.escaperoom.Config.EatenMessage
 
 object Items {
   import myModel._
 
-  def apple: SimpleFood =
+  def apple: SimpleFood = {
+
     SimpleFood(
       ItemDescription("apple", "red"),
       ItemRef(),
-      SimpleEatable(onEatExtra = state: S => state.copy(messages = Set(EatenMessage)))
+      SimpleEatable(onEatExtra =
+        Option(localState => messageLens.modify(_ :+ EatenMessage)(localState))
+      )
     )
+  }
+
 }
