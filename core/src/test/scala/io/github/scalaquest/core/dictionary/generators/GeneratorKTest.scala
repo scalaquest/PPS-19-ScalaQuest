d0d3fc4c16950b1703110b2af89bc19321681e45
Francesco Gorini <36625232+francescogorini@users.noreply.github.com>
2021-02-05 11:31:24 +0100

    Created generator tests

---
+++ core/src/test/scala/io/github/scalaquest/core/dictionary/generators/GeneratorKTest.scala
@@ -0,0 +1,25 @@
+package io.github.scalaquest.core.dictionary.generators
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class GeneratorKTest extends AnyWordSpec with Matchers {
+
+  "A GeneratorK" should {
+    implicit val generator = Generator.makeEntry((x: Int) => x -> (Math.pow(x, 2)))
+    "generate a map of entries" in {
+      val generatorK = new GeneratorK[List, Int, Map[Int, Double]]
+      generatorK.generate(List(1, 2, 3)) shouldBe Map(
+        1 -> 1,
+        2 -> 4,
+        3 -> 9
+      )
+    }
+    "generate the previous natural number" in {
+      implicit val generator = Generator.instance((x: Int) => if (x > 1) Some(x - 1) else None)
+      val generatorK         = new GeneratorK[Option, Int, Option[Int]]
+      generatorK.generate(Some(5)) shouldBe Some(4)
+      generatorK.generate(None) shouldBe None
+    }
+  }
+}
