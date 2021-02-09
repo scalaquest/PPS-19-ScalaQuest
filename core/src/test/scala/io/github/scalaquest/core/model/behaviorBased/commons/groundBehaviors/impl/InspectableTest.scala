66893b960a5979f983481ad367e41a63349d742d
ThomasAngeliniUnibo <thomas.angelini@studio.unibo.it>
2021-02-09 12:43:46 +0100

    Added Inspectable Behaviour Test

---
+++ core/src/test/scala/io/github/scalaquest/core/model/behaviorBased/commons/groundBehaviors/impl/InspectableTest.scala
@@ -0,0 +1,40 @@
+package io.github.scalaquest.core.model.behaviorBased.commons.groundBehaviors.impl
+
+import io.github.scalaquest.core.TestsUtils
+import TestsUtils.{door, key, simpleState, startRoom, targetRoom}
+import TestsUtils.model.{Inspectable, BehaviorBasedGround, GroundBehavior, Inspected}
+import io.github.scalaquest.core.model.Action.Common.Inspect
+import io.github.scalaquest.core.model.Direction
+import org.scalatest.wordspec.AnyWordSpec
+
+class InspectableTest extends AnyWordSpec {
+
+  "An Inspect Ground Behavior" when {
+    val inspection = Inspectable()
+
+    "Applied to a Ground" when {
+      case object SimpleGround extends BehaviorBasedGround {
+        override val behaviors: Seq[GroundBehavior] = Seq(inspection)
+      }
+
+      "An inspect Action is provided" should {
+        "give the opportunity to the player to see all the visible items present in the designed Room," +
+          " and show if the player could move in any direction" in {
+            val targetResult =
+              Inspected(startRoom, Set(key, door), Map(Direction.North -> targetRoom))
+
+            for {
+              react <- SimpleGround.use(Inspect)(simpleState) toRight fail(
+                "Reaction not generated"
+              )
+              modState <- Right(react(simpleState))
+
+            } yield assert(
+              modState.messages.last == targetResult,
+              "The player has reached the Room"
+            )
+          }
+      }
+    }
+  }
+}
