6d89a138c83daab87f4fa0eaa3968dba023e88eb
Riccardo Maldini <riccardo.maldini@gmail.com>
2021-02-09 01:34:37 +0100

    Sonarqube Scoverage integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,26 @@
 plugins {
     // Root-level plugin is necessary to run aggregateScoverage
     id("org.scoverage")
+
+    // SonarCloud analysis configuration
+    id("org.sonarqube")
 }
 
-subprojects {
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "scalaquest_PPS-19-ScalaQuest")
+        property("sonar.organization", "scalaquest")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.sources", "src/main/scala")
+        property("sonar.tests", "src/test/scala")
+        property("sonar.junit.reportPaths", "build/test-results/test")
+        property("sonar.scala.version", "2.13")
+        property("sonar.scala.scoverage.reportPath", "build/reports/scoverage/scoverage.xml")
+    }
 }
+
+subprojects { }
 
 buildscript { repositories { mavenCentral() }}
 
