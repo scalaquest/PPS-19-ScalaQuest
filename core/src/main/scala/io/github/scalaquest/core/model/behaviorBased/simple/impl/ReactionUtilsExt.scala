cced2a2d30eac1866d8f2c667312598e993d2e16
Lippo <filippo.nardini@studio.unibo.it>
2021-02-18 18:54:14 +0100

    Add ReactionUtilsExt tests

--- core/src/main/scala/io/github/scalaquest/core/model/behaviorBased/simple/impl/ReactionUtilsExt.scala
+++ core/src/main/scala/io/github/scalaquest/core/model/behaviorBased/simple/impl/ReactionUtilsExt.scala
@@ -13,37 +13,6 @@ trait ReactionUtilsExt extends BehaviorBasedModel {
 
   object Reaction {
 
-    def map(g: S => S)(f: Reaction): Reaction =
-      s => {
-        val (s1, m1) = f(s)
-        (g(s1), m1)
-      }
-
-    def flatMap(g: S => Reaction)(f: Reaction): Reaction = { s =>
-      val (s1, m1) = f(s)
-      val (s2, m2) = g(s1)(s1)
-      (s2, m1 ++ m2)
-    }
-
-    case object MyMessage extends Message
-
-    val aaaaa: Reaction = for {
-      s <- Reaction.empty
-      l = s.location
-      r <- if (!s.ended) Reaction.messages(MyMessage) else Reaction.empty
-
-    } yield r
-
-    def combine(a: Reaction, b: Reaction): Reaction = a.flatMap(_ => b)
-
-    def create(f: S => S, messages: Seq[Message]): Reaction = s => (f(s), messages)
-
-    def apply(f: S => S, messages: Message*): Reaction = create(f, messages)
-
-    def empty: Reaction = create(identity[S], Seq())
-
-    def messages(msgs: Message*): Reaction = appendMessages(msgs)(empty)
-
     def appendMessage(msg: Message)(r: Reaction): Reaction = appendMessages(Seq(msg))(r)
 
     def appendMessages(msgs: Seq[Message])(r: Reaction): Reaction =
@@ -52,15 +21,30 @@ trait ReactionUtilsExt extends BehaviorBasedModel {
         (s1, m1 ++ msgs)
       }
 
-    // def combine(a: Reaction, b: Reaction): Reaction = { s0 =>
-    //   val (s1, m1) = a(s0)
-    //   val (s2, m2) = b(s1)
-    //   (s2, m1 ++ m2)
-    // }
+    def apply(f: S => S, messages: Message*): Reaction = create(f, messages)
 
-    def fold(rs: Seq[Reaction]): Reaction = rs.reduceOption(combine).getOrElse(empty)
+    def combine(a: Reaction, b: Reaction): Reaction = a.flatMap(_ => b)
 
-    def foldV(rs: Reaction*): Reaction = fold(rs)
+    def create(f: S => S, messages: Seq[Message]): Reaction = s => (f(s), messages)
+
+    def empty: Reaction = create(identity[S], Seq())
+
+    def flatMap(g: S => Reaction)(f: Reaction): Reaction = { s =>
+      val (s1, m1) = f(s)
+      val (s2, m2) = g(s1)(s1)
+      (s2, m1 ++ m2)
+    }
+
+    def map(g: S => S)(f: Reaction): Reaction =
+      s => {
+        val (s1, m1) = f(s)
+        (g(s1), m1)
+      }
+
+    def messages(msgs: Message*): Reaction = appendMessages(msgs)(empty)
+
+    def pure(s: S): Reaction = create(_ => s, Seq.empty)
+
   }
 
   object Update {
