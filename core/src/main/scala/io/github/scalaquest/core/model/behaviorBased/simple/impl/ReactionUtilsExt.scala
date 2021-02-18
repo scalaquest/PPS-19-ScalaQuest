4304e93663820514b58cb7a392bad162c6ac36c9
Lippo <filippo.nardini@studio.unibo.it>
2021-02-18 17:33:08 +0100

    Flatmap reactions

--- core/src/main/scala/io/github/scalaquest/core/model/behaviorBased/simple/impl/ReactionUtilsExt.scala
+++ core/src/main/scala/io/github/scalaquest/core/model/behaviorBased/simple/impl/ReactionUtilsExt.scala
@@ -5,7 +5,36 @@ import io.github.scalaquest.core.model.behaviorBased.BehaviorBasedModel
 
 trait ReactionUtilsExt extends BehaviorBasedModel {
 
+  implicit class EnhancedReaction(r: Reaction) {
+    def map(g: S => S): Reaction = Reaction.map(g)(r)
+
+    def flatMap(g: S => Reaction): Reaction = Reaction.flatMap(g)(r)
+  }
+
   object Reaction {
+
+    def map(g: S => S)(f: Reaction): Reaction =
+      s => {
+        val (s1, m1) = f(s)
+        (g(s1), m1)
+      }
+
+    def flatMap(g: S => Reaction)(f: Reaction): Reaction = { s =>
+      val (s1, m1) = f(s)
+      val (s2, m2) = g(s1)(s1)
+      (s2, m1 ++ m2)
+    }
+
+    case object MyMessage extends Message
+
+    val aaaaa: Reaction = for {
+      s <- Reaction.empty
+      l = s.location
+      r <- if (!s.ended) Reaction.messages(MyMessage) else Reaction.empty
+
+    } yield r
+
+    def combine(a: Reaction, b: Reaction): Reaction = a.flatMap(_ => b)
 
     def create(f: S => S, messages: Seq[Message]): Reaction = s => (f(s), messages)
 
@@ -23,11 +52,11 @@ trait ReactionUtilsExt extends BehaviorBasedModel {
         (s1, m1 ++ msgs)
       }
 
-    def combine(a: Reaction, b: Reaction): Reaction = { s0 =>
-      val (s1, m1) = a(s0)
-      val (s2, m2) = b(s1)
-      (s2, m1 ++ m2)
-    }
+    // def combine(a: Reaction, b: Reaction): Reaction = { s0 =>
+    //   val (s1, m1) = a(s0)
+    //   val (s2, m2) = b(s1)
+    //   (s2, m1 ++ m2)
+    // }
 
     def fold(rs: Seq[Reaction]): Reaction = rs.reduceOption(combine).getOrElse(empty)
 
